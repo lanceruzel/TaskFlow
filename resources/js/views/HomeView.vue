86bd5d85abc534d01ddc4ef8bc2a54b07c54adb5
LanceRuzel <lanceruzel2@gmail.com>
2024-09-19 00:39:52 +0800

    conver task cards to panel

--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -3,6 +3,8 @@ import Button from 'primevue/button';
 import Card from 'primevue/card';
 import Avatar from 'primevue/avatar';
 import ScrollPanel from 'primevue/scrollpanel';
+import Panel from 'primevue/panel';
+import Checkbox from 'primevue/checkbox';
 </script>
 
 <template>
@@ -15,19 +17,32 @@ import ScrollPanel from 'primevue/scrollpanel';
                 </div>
             </template>
             <template #content>
-                <ScrollPanel class="w-full h-[63dvh]">
-                    <Card v-for="i in 20" :key="i" class="my-3 mx-1">
-                        <template #content>
-                            <div class="flex items-center justify-between">
-                                <div class="leading-tight">
-                                    <p>UI Layout</p>
-                                    <small>Frank Molina</small>
-                                </div>
-
-                                <Avatar label="10%" class="!text-xs font-bold" shape="circle" size="large" />
+                <ScrollPanel class="w-full h-[63dvh] px-1">
+                    <Panel v-for="i in 20" :key="i" class="py-3 my-3 mx-1 shadow-sm" toggleable collapsed="false">
+                        <template #header>
+                            <div class="leading-tight">
+                                <p>UI Layout</p>
+                                <small>Frank Molina</small>
                             </div>
                         </template>
-                    </Card>
+
+                        <template #icons>
+                            <Avatar label="10%" class="!text-xs font-bold !size-9 me-1" shape="circle" />
+                        </template>
+
+                        <div class="flex flex-col items-center justify-between mt-2">
+                            <ul class="w-full">
+                                <li class="py-1" v-for="i in 10" :key="i" >
+                                    <div class="flex items-center">
+                                        <Checkbox name="pizza" value="Cheese" />
+                                        <label class="ml-2">TEst</label>
+                                    </div>
+                                </li>
+                            </ul>
+
+                            <Button label="Add Task" text size="small" />
+                        </div>
+                    </Panel>
                 </ScrollPanel>
             </template>
         </Card>
@@ -39,19 +54,32 @@ import ScrollPanel from 'primevue/scrollpanel';
                 </div>
             </template>
             <template #content>
-                <ScrollPanel class="w-full h-[63dvh]">
-                    <Card v-for="i in 20" :key="i" class="my-3 mx-1">
-                        <template #content>
-                            <div class="flex items-center justify-between">
-                                <div class="leading-tight">
-                                    <p>UI Layout</p>
-                                    <small>Frank Molina</small>
-                                </div>
-
-                                <Avatar label="10%" class="!text-xs font-bold" shape="circle" size="large" />
+                <ScrollPanel class="w-full h-[63dvh] px-1">
+                    <Panel v-for="i in 20" :key="i" class="py-3 my-3 mx-1 shadow-sm" toggleable collapsed="false">
+                        <template #header>
+                            <div class="leading-tight">
+                                <p>UI Layout</p>
+                                <small>Frank Molina</small>
                             </div>
                         </template>
-                    </Card>
+
+                        <template #icons>
+                            <Avatar label="10%" class="!text-xs font-bold !size-9 me-1" shape="circle" />
+                        </template>
+
+                        <div class="flex flex-col items-center justify-between mt-2">
+                            <ul class="w-full">
+                                <li class="py-1" v-for="i in 10" :key="i" >
+                                    <div class="flex items-center">
+                                        <Checkbox name="pizza" value="Cheese" />
+                                        <label class="ml-2">TEst</label>
+                                    </div>
+                                </li>
+                            </ul>
+
+                            <Button label="Add Task" text size="small" />
+                        </div>
+                    </Panel>
                 </ScrollPanel>
             </template>
         </Card>
@@ -60,23 +88,36 @@ import ScrollPanel from 'primevue/scrollpanel';
             <template #title>
                 <div class="flex items-center justify-between">
                     <p>Done</p>
-                    <Avatar label="10%" class="!text-xs font-bold" shape="circle" />
+                    <!-- <Avatar label="10%" class="!text-xs font-bold" shape="circle" /> -->
                 </div>
             </template>
             <template #content>
-                <ScrollPanel class="w-full h-[63dvh]">
-                    <Card v-for="i in 20" :key="i" class="my-3 mx-1">
-                        <template #content>
-                            <div class="flex items-center justify-between">
-                                <div class="leading-tight">
-                                    <p>UI Layout</p>
-                                    <small>Frank Molina</small>
-                                </div>
-
-                                <Avatar label="10%" class="!text-xs font-bold" shape="circle" size="large" />
+                <ScrollPanel class="w-full h-[63dvh] px-1">
+                    <Panel v-for="i in 20" :key="i" class="py-3 my-3 mx-1 shadow-sm" toggleable collapsed="false">
+                        <template #header>
+                            <div class="leading-tight">
+                                <p>UI Layout</p>
+                                <small>Frank Molina</small>
                             </div>
                         </template>
-                    </Card>
+
+                        <template #icons>
+                            <Avatar label="10%" class="!text-xs font-bold !size-9 me-1" shape="circle" />
+                        </template>
+
+                        <div class="flex flex-col items-center justify-between mt-2">
+                            <ul class="w-full">
+                                <li class="py-1" v-for="i in 10" :key="i" >
+                                    <div class="flex items-center">
+                                        <Checkbox name="pizza" value="Cheese" />
+                                        <label class="ml-2">TEst</label>
+                                    </div>
+                                </li>
+                            </ul>
+
+                            <Button label="Add Task" text size="small" />
+                        </div>
+                    </Panel>
                 </ScrollPanel>
             </template>
         </Card>
