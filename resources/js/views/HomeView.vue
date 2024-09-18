5442debfaa3d523f868e19ed49df6d92b1915e78
LanceRuzel <lanceruzel2@gmail.com>
2024-09-18 12:14:05 +0800

    update task cards paddings

--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -8,7 +8,7 @@ import ScrollPanel from 'primevue/scrollpanel';
 
 <template>
     <div class="grid grid-cols-3 gap-5 absolute top-0 bottom-0 right-0 left-0 p-5">
-        <Card class="grid-cols-1 cursor-pointer h-full">
+        <Card class="grid-cols-1 cursor-pointer h-full -mb-5">
             <template #title>
                 <div class="flex items-center justify-between">
                     <p>Assigned</p>
@@ -16,7 +16,7 @@ import ScrollPanel from 'primevue/scrollpanel';
                 </div>
             </template>
             <template #content>
-                <ScrollPanel class="w-full h-[calc(100dvh-16.3rem)]">
+                <ScrollPanel class="w-full h-[63dvh]">
                     <Card v-for="i in 20" :key="i" class="my-3 mx-1">
                         <template #content>
                             <div class="flex items-center justify-between">
@@ -33,14 +33,14 @@ import ScrollPanel from 'primevue/scrollpanel';
             </template>
         </Card>
 
-        <Card class="grid-cols-1 cursor-pointer h-full">
+        <Card class="grid-cols-1 cursor-pointer h-full -mb-5">
             <template #title>
                 <div class="flex items-center justify-between">
                     <p>In Progress</p>
                 </div>
             </template>
             <template #content>
-                <ScrollPanel class="w-full h-[calc(100dvh-16.3rem)]">
+                <ScrollPanel class="w-full h-[63dvh]">
                     <Card v-for="i in 20" :key="i" class="my-3 mx-1">
                         <template #content>
                             <div class="flex items-center justify-between">
@@ -57,7 +57,7 @@ import ScrollPanel from 'primevue/scrollpanel';
             </template>
         </Card>
 
-        <Card class="grid-cols-1 cursor-pointer h-full">
+        <Card class="grid-cols-1 cursor-pointer h-full -mb-5">
             <template #title>
                 <div class="flex items-center justify-between">
                     <p>Done</p>
@@ -65,27 +65,7 @@ import ScrollPanel from 'primevue/scrollpanel';
                 </div>
             </template>
             <template #content>
-                <ScrollPanel class="w-full h-[calc(100dvh-16.3rem)]">
-                    <Card v-for="i in 20" :key="i" class="my-3 mx-1">
-                        <template #content>
-                            <div class="flex items-center justify-between">
-                                <div class="leading-tight">
-                                    <p>UI Layout</p>
-                                    <small>Frank Molina</small>
-                                </div>
-
-                                <Avatar label="10%" class="!text-xs font-bold" shape="circle" size="large" />
-                            </div>
-                        </template>
-                    </Card>
-                </ScrollPanel>
-            </template>
-        </Card>
-
-        <Card class="grid-cols-1 cursor-pointer h-full">
-            <template #title>Assigned</template>
-            <template #content>
-                <ScrollPanel class="w-full h-[calc(100dvh-16.3rem)]">
+                <ScrollPanel class="w-full h-[63dvh]">
                     <Card v-for="i in 20" :key="i" class="my-3 mx-1">
                         <template #content>
                             <div class="flex items-center justify-between">
