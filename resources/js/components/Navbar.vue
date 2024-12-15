948d0c5bd98121cb71f27f7c18ef5be530661bc1
LanceRuzel <lanceruzel2@gmail.com>
2024-12-16 01:02:18 +0800

    added project title on navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,38 +1,23 @@
 <script setup>
-import Avatar from 'primevue/avatar';
 import ProfileMenu from './ProfileMenu.vue';
 import NotificationPopover from './NotificationPopover.vue';
-import AddProjectDialog from './AddProjectDialog.vue';
-import ScrollPanel from 'primevue/scrollpanel';
 
 import { useProjectStore } from '../stores/project';
 import { onMounted, ref } from 'vue';
 import { storeToRefs } from 'pinia';
 
 const projectStore = useProjectStore();
-const { getProjects, getSelectedProject } = projectStore;
-const { selectedProject, projects } = storeToRefs(projectStore);
-
-const loadDefaultSelectedProject = () => {
-    if(selectedProject.value === null){
-        if(projects.value[0]){
-            getSelectedProject(projects.value[0].id);
-        }
-    }
-}
-
-onMounted(async () => {
-    await getProjects();
-
-    loadDefaultSelectedProject();
-});
 </script>
 
 <template>
-    <div class="w-full border-b bg-white transition-all p-3 flex items-center justify-end">
-        <nav class="flex items-center justify-start gap-3 px-3">
+    <nav class="bg-white flex items-center justify-between gap-3 px-5 py-4">
+        <div>
+            <p v-if="projectStore.selectedProject" class="text-lg font-semibold">Project: <span class="font-normal" v-text="projectStore.selectedProject.project_title"></span></p>
+        </div>
+
+        <div class="flex items-center justify-center">
             <NotificationPopover/>
             <ProfileMenu />
-        </nav>
-    </div>
+        </div>
+    </nav>
 </template>
